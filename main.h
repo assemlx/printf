a0d3ba48050e8f06c7100bdcff5f7cef645f2820
Assem <asemstudy86@gmail.com>
2023-08-20 15:09:26 -0700

    00main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int print_number(va_list args);
+int print_binary(va_list args);
+
+#endif
