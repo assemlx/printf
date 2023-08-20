fbb7ba42ae8c56670a9b50ad56507fbd65f10b5d
Assem <asemstudy86@gmail.com>
2023-08-20 15:24:35 -0700

    01printf

--- printf.c
+++ printf.c
@@ -36,3 +36,40 @@ int print_number(va_list args)
 
 	return (len);
 }
+
+/**
+ * print_binary - print an unsigned int as binary
+ * @args: the va_list containing the number to print
+ *
+ * Return: the number of characters printed
+ */
+int print_binary(va_list args)
+{
+	unsigned int num = va_arg(args, unsigned int);
+	int len = 0;
+	int i;
+	char buffer[32];
+
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+
+	for (i = 0; i < 32; i++)
+		buffer[i] = (num >> (31 - i) & 1) + '0';
+
+	for (i = 0; i < 32; i++)
+	{
+		if (buffer[i] == '1')
+			break;
+	}
+
+	for (; i < 32; i++)
+	{
+		_putchar(buffer[i]);
+		len++;
+	}
+
+	return (len);
+}
